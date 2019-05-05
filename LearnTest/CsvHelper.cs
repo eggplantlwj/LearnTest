e430c76c1938b65a7d2338887d43cb2b83ff1fba
liu.wenjie <wenjie.liu@leadchina.cn>
2019-05-05 10:31:00 +0800

    添加项目文件。

---
+++ LearnTest/CsvHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HIX
+{
+    public class CsvHelper
+    {
+        /// <summary>
+        /// string导出为CSV
+        /// </summary>
+        /// <param name="dt">DataTable</param>
+        /// <param name="strFilePath">路径</param>
+        public static void StringToCsv(string data, string strFilePath)
+        {
+            FileStream fs = new FileStream(strFilePath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(data);
+            sw.Close();
+            fs.Close();
+        }
+
+
+
+        /// <summary>
+        /// 写入CSV
+        /// </summary>
+        /// <param name="dt">DataTable</param>
+        /// <param name="strFilePath">路径</param>
+        public static void dataTableToCsvT(System.Data.DataTable dt, string strFilePath)
+        {
+            if (dt == null || dt.Rows.Count == 0)   //确保DataTable中有数据
+                return;
+            string strBufferLine = "";
+            StreamWriter strmWriterObj = new StreamWriter(strFilePath, false, System.Text.Encoding.Default);
+            //写入列头
+            foreach (System.Data.DataColumn col in dt.Columns)
+                strBufferLine += col.ColumnName + ",";
+            strBufferLine = strBufferLine.Substring(0, strBufferLine.Length - 1);
+            strmWriterObj.WriteLine(strBufferLine);
+            //写入记录
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                strBufferLine = "";
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    if (j > 0)
+                        strBufferLine += ",";
+                    strBufferLine += dt.Rows[i][j].ToString().Replace(",", "");   //因为CSV文件以逗号分割，在这里替换为空
+                }
+                strmWriterObj.WriteLine(strBufferLine);
+            }
+            strmWriterObj.Close();
+        }
+
+
+        /// <summary>
+        /// 读取CSV
+        /// </summary>
+        /// <param name="filePath">CSV路径</param>
+        /// <param name="n">表示第n行是字段title,第n+1行是记录开始</param>
+        /// <returns></returns>
+        public static System.Data.DataTable CsvToDataTable(string filePath, int n)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            StreamReader reader = new StreamReader(filePath, System.Text.Encoding.Default, false);
+            int m = 0;
+            while (!reader.EndOfStream)
+            {
+                m = m + 1;
+                string str = reader.ReadLine();
+                string[] split = str.Split(',');
+                if (m == n)
+                {
+                    System.Data.DataColumn column; //列名
+                    for (int c = 0; c < split.Length; c++)
+                    {
+                        column = new System.Data.DataColumn();
+                        column.DataType = System.Type.GetType("System.String");
+                        column.ColumnName = split[c];
+                        if (dt.Columns.Contains(split[c]))                 //重复列名处理
+                            column.ColumnName = split[c] + c;
+                        dt.Columns.Add(column);
+                    }
+                }
+                if (m >= n + 1)
+                {
+                    System.Data.DataRow dr = dt.NewRow();
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        dr[i] = split[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            reader.Close();
+            return dt;
+        }
+
+    }
+}
---
+++ LearnTest/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HIX;
+
+namespace LearnTest
+{
+    public partial class MainForm : Form
+    {
+
+        public static MainForm instance = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        public static MainForm GetInstance()
+        {
+            if(instance != null)
+            {
+                return instance;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Publisher myPublisher = new Publisher();
+            Subscriber mySubscriber_1 = new Subscriber_1(myPublisher);
+            Subscriber mySubscriber_2 = new Subscriber_2(myPublisher);
+
+            myPublisher.SendMsgSpecific("News Appear");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ChangeUIClass myChangeUIClass = new ChangeUIClass();
+            //myChangeUIClass.changeUIEventHandler += new ChangeUIDelegate(ChangeUI);
+
+            //myChangeUIClass.ChangeUIFun(MainForm.GetInstance().label1, "改变");
+            TaskRun2.eventHandlerChangeUI += new EventHandler(ChangeUIFun);
+            TaskRun2.Start2();
+            
+        }
+
+        public void ChangeUIFun(object sender, EventArgs e)
+        {
+            Hanlder myHandler = e as Hanlder;
+            label1.Text = myHandler.myString;
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            string path = "D:FAI.csv";
+            CsvHelper.StringToCsv(this.textBox1.Text, path);
+            
+        }
+    }
+
+
+
+
+
+
+    public delegate void ChangeUIDelegate(Control uiControl, object obj);
+    public class ChangeUIClass
+    {
+        public event ChangeUIDelegate changeUIEventHandler;
+
+        public void ChangeUIFun(Control uiControl, object obj)
+        {
+            changeUIEventHandler?.Invoke(uiControl, obj);
+        }
+
+        public void ChangeUI(Control myControl, object obj)
+        {
+            if(myControl.InvokeRequired)
+            {
+                ChangeUIDelegate myChangeUIDelegate = new ChangeUIDelegate(ChangeUI);
+                myControl.Invoke(myChangeUIDelegate, new object[] { myControl, obj });
+            }
+           else
+            {
+                SpecificChange(myControl, obj);
+            }
+        }
+
+        public void SpecificChange(Control myControl, object obj)
+        {
+            myControl.Text = obj.ToString();
+        }
+    }
+
+
+    public class TaskRun
+    {
+        
+        public static void Start2()
+        {
+            Task myRun = new Task(() => Start());
+            myRun.Start();
+        }
+
+        public static void Start()
+        {
+            ChangeUIClass myChangeUIClass = new ChangeUIClass();
+            //myChangeUIClass.changeUIEventHandler += new ChangeUIDelegate(myChangeUIClass.ChangeUI);
+
+            myChangeUIClass.ChangeUI(MainForm.GetInstance().label1, "改变");
+        }
+    }
+
+    public class TaskRun2
+    {
+        public delegate void ChangeUI(Control myControl, object obj);
+        public static EventHandler eventHandlerChangeUI;
+
+        public static void Start2()
+        {
+            Task myRun = new Task(() => Start());
+            myRun.Start();
+        }
+
+        public static void Start()
+        {
+            if (eventHandlerChangeUI != null)
+            {
+                Hanlder myHandler = new Hanlder();
+                myHandler.myControl = MainForm.GetInstance().label1;
+                myHandler.myString = "事件改变";
+                eventHandlerChangeUI(null, myHandler);
+            }
+        }
+    }
+
+    public class Hanlder : EventArgs
+    {
+        public Control myControl { get; set; }
+        public string myString { get; set; }
+    }
+
+    public delegate void SendMsg(string msg);
+    public class Publisher
+    {
+        public event SendMsg eventHandle;
+
+        public void SendMsgSpecific(string msg)
+        {
+            MessageBox.Show("发布消息：" + msg);
+            eventHandle?.Invoke(msg);
+        }
+    }
+
+
+    interface Subscriber
+    {
+        void AcceptMsg(string msg);
+    }
+
+    public class Subscriber_1: Subscriber
+    {
+        public Subscriber_1(Publisher myPublisher)
+        {
+            myPublisher.eventHandle += new SendMsg(AcceptMsg);
+            myPublisher.eventHandle += new SendMsg(AcceptMsg2);
+        }
+
+        public void AcceptMsg(string msg)
+        {
+            MessageBox.Show("订阅者1接收消息1：" + msg);
+        }
+
+        public void AcceptMsg2(string msg)
+        {
+            MessageBox.Show("订阅者1接收消息2：" + msg);
+        }
+    }
+
+    public class Subscriber_2: Subscriber
+    {
+        public Subscriber_2(Publisher myPublisher)
+        {
+            myPublisher.eventHandle += new SendMsg(AcceptMsg);
+        }
+
+        public void AcceptMsg(string msg)
+        {
+            MessageBox.Show("订阅者2接收消息：" + msg);
+        }
+    }
+}
